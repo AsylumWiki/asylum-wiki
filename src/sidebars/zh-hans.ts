6ee3256f6dac07dc403ae7d02e8382013e963a1c
Linzh <linzh7@yahoo.com>
2024-08-10 15:53:41 +0300

    Add sidebar, zh-hans

--- src/sidebars/zh-hans.ts
+++ src/sidebars/zh-hans.ts
@@ -1,3 +1,18 @@
 export default [
-  
-]
+  {
+    text: "导读",
+    items: [{ text: "资源整合", link: "/zh-hans/refs" }],
+  },
+  {
+    text: "欧洲",
+    items: [{ text: "欧洲总览", link: "/zh-hans/eu" }],
+  },
+  {
+    text: "亚洲",
+    items: [{ text: "新加坡", link: "/zh-hans/as/sg" }],
+  },
+  {
+    text: "大洋洲",
+    items: [{ text: "澳大利亚", link: "/zh-hans/oc/au" }],
+  },
+];
